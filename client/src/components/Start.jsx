f5a5ffdf6502e54fc07e6701c59230d5c5526bc8
prajwal <prajwallakra05@gmail.com>
2025-02-11 20:12:21 +0530

    improved the design of home page

--- client/src/components/Start.jsx
+++ client/src/components/Start.jsx
@@ -3,10 +3,13 @@ import React from 'react';
 export default function Start() {
     return (
         <React.Fragment>
-            <div className="container text-white">
-                <h1>Sketch Verse</h1>
-                <h3>Sketch, play, and express, because creativity has no limits on Sketch verse!</h3>
-                <p>A collaborative website where you can draw and create alongside others on a shared canvas.</p>
+            <div className="flex items-center justify-center h-screen text-white">
+                <div className='text-2xl text-center'>
+                    <h1 className='text-6xl text-lime-500 font-bold p-6'>Sketch Verse</h1>
+                    <h3 className='text-4xl pb-5'>Sketch, play, and express, because creativity has no limits on Sketch verse!</h3>
+                    <p>A collaborative website where you can draw and create alongside others on a shared canvas.</p>
+                </div>
+                
             </div>
         </React.Fragment>
     );
