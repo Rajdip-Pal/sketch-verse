b890177ced64d65081251598b58ce5136cccd688
prajwal <prajwallakra05@gmail.com>
2025-02-11 19:42:24 +0530

    added background image

--- client/src/components/Start.jsx
+++ client/src/components/Start.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Start() {
     return (
         <React.Fragment>
-            <div className="container text-blue">
+            <div className="container text-white">
                 <h1>Sketch Verse</h1>
                 <h3>Sketch, play, and express, because creativity has no limits on Sketch verse!</h3>
                 <p>A collaborative website where you can draw and create alongside others on a shared canvas.</p>
