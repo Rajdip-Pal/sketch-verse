cf4c9775a85ff28dd9f528d81c1e7dbac979b0aa
prajwal <prajwallakra05@gmail.com>
2025-02-12 00:28:44 +0530

    added final home page

--- client/src/components/Start.jsx
+++ client/src/components/Start.jsx
@@ -5,9 +5,11 @@ export default function Start() {
         <React.Fragment>
             <div className="flex items-center justify-center h-screen text-white">
                 <div className='text-2xl text-center'>
-                    <h1 className='text-6xl text-lime-500 font-bold p-6'>Sketch Verse</h1>
-                    <h3 className='text-4xl pb-5'>Sketch, play, and express, because creativity has no limits on Sketch verse!</h3>
-                    <p>A collaborative website where you can draw and create alongside others on a shared canvas.</p>
+                    <h1 className='text-6xl text-lime-500 font-bold p-6 font-kumar'>Sketch Verse</h1>
+                    <p className="text-5xl pb-2 font-kotta">Sketch, play, and express, because creativity has no limits on 
+                    <span className="text-lime-500 px-2 rounded-md">Sketch verse!</span>
+                    </p>
+                    <p className='text-gray-400'>A collaborative website where you can draw and create alongside others on a shared canvas.</p>
                 </div>
                 
             </div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,6 +5,7 @@ function Home() {
     return (
         <React.Fragment>
             <Start />
+            <button className='px-8 py-2 absolute bottom-4 right-4 font-kotta text-3xl rounded-3xl bg-lime-500 text-black hover:bg-green-700 hover:text-white active:scale-90 '>Start tour</button>
         </React.Fragment>
     );
 }
