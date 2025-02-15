643685b4fabb77b652b643ca3b1f5f4003436578
Rajdip-Pal <rajdippal2005@gmail.com>
2025-02-15 21:47:42 +0530

    Minor changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-export function FixedTopRight({ path, children }) {
+export function FixedTopRight({ path, headline, children }) {
     return (
         <React.Fragment>
-            <nav className="md:flex hidden justify-between">
-                <div></div>
+            <nav className="md:flex hidden justify-between absolute w-full top-0 right-0">
+                <div className="p-6 md:mx-5 md:my-5">{children}</div>
                 <Link to={path}>
-                    <div className="xl:text-3xl md:text-2xl text-4xl text-lime-500 font-bold p-6 font-kumar md:mx-5 my-5">{children}</div>
+                    <div className="xl:text-3xl md:text-2xl text-4xl text-lime-500 font-bold p-6 font-kumar md:mx-5 my-5">{headline}</div>
                 </Link>
             </nav>
-            <nav className="flex md:hidden justify-between">
-                <div></div>
+            <nav className="flex md:hidden justify-between absolute w-full top-0 right-0">
+                <div className="p-6 md:mx-5 md:my-5">{children}</div>
                 <Link to={path}>
-                    <div className="text-lime-500 font-bold p-6 font-kumar md:mx-5 md:my-5">{children}</div>
+                    <div className="text-lime-500 font-bold p-6 font-kumar md:mx-5 md:my-5">{headline}</div>
                 </Link>
             </nav>
         </React.Fragment>
@@ -23,26 +23,29 @@ export function FixedTopRight({ path, children }) {
 
 FixedTopRight.propTypes = {
     path: PropTypes.string,
+    headline: PropTypes.oneOfType([PropTypes.node, PropTypes.string]),
     children: PropTypes.oneOfType([PropTypes.node, PropTypes.string]),
 };
 
 FixedTopRight.defaultProps = {
     path: '/',
-    children: 'Sketc Verse',
+    headline: 'Sketc Verse',
 };
 
-export function FixedTopLeft({ path, children }) {
+export function FixedTopLeft({ path, headline, children }) {
     return (
         <React.Fragment>
-            <nav className="md:flex hidden justify-between">
+            <nav className="md:flex hidden justify-between absolute w-full top-0 left-0">
                 <Link to={path}>
-                    <div className="xl:text-3xl md:text-2xl text-4xl text-lime-500 font-bold p-6 font-kumar md:mx-5 my-5">{children}</div>
+                    <div className="xl:text-3xl md:text-2xl text-4xl text-lime-500 font-bold p-6 font-kumar md:mx-5 my-5">{headline}</div>
                 </Link>
+                <div className="p-6 md:mx-5 md:my-5">{children}</div>
             </nav>
-            <nav className="flex md:hidden justify-between">
+            <nav className="flex md:hidden justify-between absolute w-full top-0 left-0">
                 <Link to={path}>
-                    <div className="text-lime-500 font-bold p-6 font-kumar md:mx-5 md:my-5">{children}</div>
+                    <div className="text-lime-500 font-bold p-6 font-kumar md:mx-5 md:my-5">{headline}</div>
                 </Link>
+                <div className="p-6 md:mx-5 md:my-5">{children}</div>
             </nav>
         </React.Fragment>
     );
@@ -50,10 +53,11 @@ export function FixedTopLeft({ path, children }) {
 
 FixedTopLeft.propTypes = {
     path: PropTypes.string,
+    headline: PropTypes.oneOfType([PropTypes.node, PropTypes.string]),
     children: PropTypes.oneOfType([PropTypes.node, PropTypes.string]),
 };
 
 FixedTopLeft.defaultProps = {
     path: '/',
-    children: 'Sketc Verse',
+    headline: 'Sketc Verse',
 };
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,37 +1,92 @@
+// import dependencies
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
-function Home() {
-    document.title = 'Sketch Verse';
+// import Components
+import * as Navbar from '../components/Navbar';
+
+/**
+ * Home component that renders the main page of the Sketch Verse application.
+ * @returns {JSX.Element} The JSX element representing the home page.
+ */
+export default function Home() {
+    React.useEffect(() => {
+        // Update the document title when the component mounts
+        document.title = 'Sketch Verse';
+    }, []);
 
     return (
+        // Container element that centers the content vertically and horizontally
         <React.Fragment>
-            <div className="flex items-center justify-center h-screen text-white relative">
+            {/* For Devices with a screen width of 768px or more */}
+            <div className="hidden md:flex items-center justify-center h-screen text-white ">
                 <div className="text-2xl text-center">
-                    <motion.h1 className="text-6xl text-lime-500 font-bold p-6 font-kumar">Sketch Verse</motion.h1>
+                    {/* Main title with animation */}
+                    <motion.div
+                        initial={{ opacity: 0, y: 20 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 1 }}
+                        className="text-6xl text-lime-500 font-bold p-3 font-kumar m-2">
+                        Sketch Verse
+                    </motion.div>
 
-                    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
+                    <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }}>
+                        {/* Tagline with a call-to-action */}
                         <p className="text-5xl pb-7 font-kotta">
-                            <div className="pb-2">Sketch, play, and express,</div>
+                            <span className="pb-2 block">Sketch, play, and express,</span>
                             because creativity has no limits on
-                            <span className="text-lime-500 px-2 rounded-md">Sketch verse!</span>
+                            <span className="text-lime-500 px-2">Sketch verse!</span>
                         </p>
+
+                        {/* Description of the Sketch Verse application */}
                         <p className="text-gray-400 font-nixie font-bold">
-                            <div>A collaborative website where you can draw</div>
+                            <span className="block">A collaborative website where you can draw</span>
                             and create alongside others on a shared canvas.
                         </p>
                     </motion.div>
 
+                    {/* Start Tour button with link */}
                     <Link to={'/start'}>
-                        <button className="px-10 py-2 absolute bottom-8 right-8 font-kotta text-3xl rounded-3xl bg-lime-500 text-black hover:bg-green-700 hover:text-white active:scale-90">
+                        <button
+                            className="px-10 py-2 absolute bottom-8 right-8 font-kotta text-3xl rounded-3xl bg-lime-500 text-black hover:bg-green-700 hover:text-white active:scale-90"
+                            aria-label="Start the Sketch Verse tour">
                             Start tour
                         </button>
                     </Link>
                 </div>
             </div>
+            {/* For Devices with a screen width of 768px or less */}
+            <div className="flex flex-col md:hidden h-screen align-middle items-center justify-center">
+                <Navbar.FixedTopLeft headline={'Sketch Verse'} path={'/'} />
+
+                <motion.div
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.8 }}
+                    className="flex flex-col items-center align-middle justify-center">
+                    {/* Tagline with a call-to-action */}
+                    <p className="text-xl pb-7 font-kotta text-white">
+                        Sketch, play, and express, because creativity has no limits on
+                        <strong className="text-lime-500 px-2">Sketch verse!</strong>
+                    </p>
+
+                    {/* Description of the Sketch Verse application */}
+                    <p className="text-gray-400 font-nixie font-bold">
+                        <span className="block">A collaborative website where you can draw</span>
+                        and create alongside others on a shared canvas.
+                    </p>
+                </motion.div>
+
+                {/* Start Tour button with link */}
+                <Link to={'/start'}>
+                    <button
+                        className="px-10 py-2  bottom-8 right-8 font-kotta text-3xl rounded-3xl bg-lime-500 text-black hover:bg-green-700 hover:text-white active:scale-90"
+                        aria-label="Start the Sketch Verse tour">
+                        Start tour
+                    </button>
+                </Link>
+            </div>
         </React.Fragment>
     );
 }
-
-export default Home;
