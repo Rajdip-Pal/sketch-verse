87fd1582fd3da792dc0abb3220d5210842432f6e
Rajdip-Pal <rajdippal2005@gmail.com>
2025-02-16 15:45:56 +0530

    UI for devices updates

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -6,25 +6,26 @@ import { motion } from 'framer-motion';
 export default function Card({ image, imageAbout, imageWidth, heading, content, buttonField, buttonLink }) {
     return (
         <motion.div
-            className="backdrop-blur-xl flex flex-col justify-center border-4 border-lime-500 rounded-xl p-5 md:w-1/2 lg:w-1/2 xl:w-1/4 m-10  shadow-2xl shadow-black"
+            className="flex flex-col justify-center border-4 border-lime-500 rounded-xl p-5 md:w-[40%] lg:w-[50%] 2xl:w-[25%] m-10  shadow-2xl shadow-black"
             initial={{ backdropFilter: 'blur(0px)' }}
             animate={{ backdropFilter: 'blur(10px)' }}
             transition={{ duration: 4, delay: 0.01, ease: 'easeOut' }}
             whileHover={{ scale: 1.05, transition: { duration: 0.5 } }}>
             <div className="flex justify-center my-8 h-[50%]">
-                <img className="rounded-xl w-[90%]  border-4 border-lime-500" src={image} alt={`${imageAbout}`} width={imageWidth} />
+                <img className="rounded-xl w-[90%] border-4 border-lime-500" src={image} alt={`${imageAbout}`} width={imageWidth} />
             </div>
             <div className="align-super">
-                <h1 className="text-3xl text-lime-500 font-bold font-eater">{heading}</h1>
-                <p className="text-left m-4 text-gray-300 text-lg">{content}</p>
+                <h1 className="text-3xl text-center text-lime-500 font-bold font-eater">{heading}</h1>
+                <p className="text-left m-4 text-gray-300 text-s md:text-lg font-kota">{content}</p>
             </div>
-            <div className="mt-auto flex justify-center">
+
+            <motion.div className="mt-auto flex justify-center">
                 <Link to={buttonLink}>
-                    <button className="w-[100%] mb-5 px-10 py-2 text-wrap font-kota text-3xl rounded-3xl bg-lime-500 text-black hover:bg-green-700 hover:text-white active:scale-90 ">
+                    <motion.button className="w-[100%] md-3 mt-3 md:mb-5 px-10 py-2 text-wrap font-kota text-xl md:text-3xl rounded-3xl bg-lime-500 text-black hover:bg-green-700 hover:text-white active:scale-90 ">
                         {buttonField}
-                    </button>
+                    </motion.button>
                 </Link>
-            </div>
+            </motion.div>
         </motion.div>
     );
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-export function FixedTopRight({ path, headline, children }) {
+export function FixedTopRight({ id, className, path, headline, children }) {
     return (
         <React.Fragment>
-            <nav className="md:flex hidden justify-between absolute w-full top-0 right-0">
+            <nav id={id} className={` ${className} md:flex hidden justify-between absolute w-full top-0 right-0`}>
                 <div className="p-6 md:mx-5 md:my-5">{children}</div>
                 <Link to={path}>
                     <div className="xl:text-3xl md:text-2xl text-4xl text-lime-500 font-bold p-6 font-eater md:mx-5 my-5">{headline}</div>
                 </Link>
             </nav>
-            <nav className="flex md:hidden justify-between absolute w-full top-0 right-0">
+            <nav id={id} className={` ${className} flex md:hidden justify-between absolute w-full top-0 right-0`}>
                 <div className="p-6 md:mx-5 md:my-5">{children}</div>
                 <Link to={path}>
                     <div className="text-lime-500 font-bold p-6 font-eater md:mx-5 md:my-5">{headline}</div>
@@ -22,6 +22,8 @@ export function FixedTopRight({ path, headline, children }) {
 }
 
 FixedTopRight.propTypes = {
+    id: PropTypes.string,
+    className: PropTypes.string,
     path: PropTypes.string,
     headline: PropTypes.oneOfType([PropTypes.node, PropTypes.string]),
     children: PropTypes.oneOfType([PropTypes.node, PropTypes.string]),
@@ -32,16 +34,16 @@ FixedTopRight.defaultProps = {
     headline: 'Sketc Verse',
 };
 
-export function FixedTopLeft({ path, headline, children }) {
+export function FixedTopLeft({ id, className, path, headline, children }) {
     return (
         <React.Fragment>
-            <nav className="md:flex hidden justify-between absolute w-full top-0 left-0">
+            <nav id={id} className={`${className} md:flex hidden justify-between absolute w-full top-0 left-0`}>
                 <Link to={path}>
                     <div className="xl:text-3xl md:text-2xl text-4xl text-lime-500 font-bold p-6 font-eater md:mx-5 my-5">{headline}</div>
                 </Link>
                 <div className="p-6 md:mx-5 md:my-5">{children}</div>
             </nav>
-            <nav className="flex md:hidden justify-between absolute w-full top-0 left-0">
+            <nav id={id} className={` ${className} flex md:hidden justify-between absolute w-full top-0 left-0`}>
                 <Link to={path}>
                     <div className="text-lime-500 font-bold p-6 font-eater md:mx-5 md:my-5">{headline}</div>
                 </Link>
@@ -52,6 +54,8 @@ export function FixedTopLeft({ path, headline, children }) {
 }
 
 FixedTopLeft.propTypes = {
+    id: PropTypes.string,
+    className: PropTypes.string,
     path: PropTypes.string,
     headline: PropTypes.oneOfType([PropTypes.node, PropTypes.string]),
     children: PropTypes.oneOfType([PropTypes.node, PropTypes.string]),
