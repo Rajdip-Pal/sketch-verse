b50d7064bc798885b42faa6b47bb56c24ba59971
Rajdip-Pal <rajdippal2005@gmail.com>
2025-02-17 15:18:07 +0530

    .

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -3,7 +3,7 @@ import CanvasTool from './CanvasTool';
 import { io } from 'socket.io-client';
 import { useParams } from 'react-router-dom';
 
-const socket = io('http://192.168.0.104:5000');
+const socket = io(process.env.SERVER);
 
 const Canvas = ({ className, localStorageId, width = 800, height = 500, darkMode = false }) => {
     const { roomId } = useParams();
